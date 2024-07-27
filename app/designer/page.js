fd14a259a7b9736bf1ef6160fc0b9443f6ffb392
mohamad.alturky <m799678@gmail.com>
2024-07-27 23:18:01 +0300

    adding the designer page

---
+++ app/designer/page.js
@@ -0,0 +1,8 @@
+'use client'
+import '@xyflow/react/dist/style.css';
+import OverviewFlow from "../../design/OverviewFlow"
+export default function Projects() {
+  return (
+      <OverviewFlow />
+  );
+}
