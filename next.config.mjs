d70c67a1a34677303c35adcbef0fff09e6a0dbff
mohamad.alturky <mohamad.alturky@hiast.edu.sy>
2024-08-26 09:15:08 +0300

    editconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-import createMDX from '@next/mdx'
- 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
-}
+const nextConfig = {};
+
+export default nextConfig;
+
+// import createMDX from '@next/mdx'
  
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-})
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // Configure `pageExtensions` to include markdown and MDX files
+//   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+//   // Optionally, add any other Next.js config below
+// }
  
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+// const withMDX = createMDX({
+//   // Add markdown plugins here, as desired
+// })
+ 
+// // Merge MDX config with Next.js config
+// export default withMDX(nextConfig)
